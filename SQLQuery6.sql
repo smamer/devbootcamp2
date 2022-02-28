7f3637a1a94014b656ed09b850ab9c4e8bd706f4
smamer <smamer@milestechnologies.com>
2022-02-28 09:04:27 -0600

    Primary/Foreign Key additions

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -1,36 +1,50 @@
-DROP TABLE IF EXISTS [Services];
-CREATE TABLE [Services](
-    ServiceID INT IDENTITY(1, 1),
-    ServiceName varchar(250),
-    StatusID INT
+DROP TABLE IF EXISTS [Role];
+CREATE TABLE [Role](
+	RoleID INT PRIMARY KEY IDENTITY(1, 1),
+	RoleName varchar(250)
 );
-
+DROP TABLE IF EXISTS [Supply];
+CREATE TABLE [Supply](
+	SupplyID INT PRIMARY KEY IDENTITY(1, 1),
+	SupplyName varchar(250),
+	SupplyCost MONEY
+);
 DROP TABLE IF EXISTS [ServiceStatus];
 CREATE TABLE [ServiceStatus](
-    StatusID INT IDENTITY(1, 1),
+    StatusID INT PRIMARY KEY IDENTITY(1, 1),
     Status varchar(250)
 );
-
+DROP TABLE IF EXISTS [Services];
+CREATE TABLE [Services](
+    ServiceID INT PRIMARY KEY IDENTITY(1, 1),
+    ServiceName varchar(250),
+    StatusID INT FOREIGN KEY REFERENCES ServiceStatus(StatusID)
+);
+DROP TABLE IF EXISTS [ServiceSupplies];
+CREATE TABLE [ServiceSupplies](
+	SupplyID INT FOREIGN KEY REFERENCES Supply(SupplyID),
+	ServiceID INT FOREIGN KEY REFERENCES Services(ServiceID)
+);
 DROP TABLE IF EXISTS [Sales];
 CREATE TABLE [Sales](
-    ServiceID INT,
+    ServiceID INT FOREIGN KEY REFERENCES Services(ServiceID),
     Guest varchar(250),
-    Price INT,
+    Price MONEY,
     DatePurchased DATE,
     AmountPurchased INT,
     TavernID INT
 );
 DROP TABLE IF EXISTS [GoodsReceived];
 CREATE TABLE [GoodsReceived](
-    SupplyID INT,
+    SupplyID INT FOREIGN KEY REFERENCES Supply(SupplyID),
     TavernID INT,
-    Cost INT,
+    Cost MONEY,
     Amount INT,
     Date DATETIME
 );
 DROP TABLE IF EXISTS [Inventory];
 CREATE TABLE [Inventory](
-    SupplyID INT,
+    SupplyID INT FOREIGN KEY REFERENCES Supply(SupplyID),
     TavernID INT,
     DateOfInventory DATETIME,
     Quantity INT
@@ -39,30 +53,58 @@ DROP TABLE IF EXISTS [Users];
 CREATE TABLE [Users](
     UserID INT IDENTITY(1,1),
     UserName varchar(250),
-    RoleID INT
+    RoleID INT FOREIGN KEY REFERENCES Role(RoleID)
 );
 DROP TABLE IF EXISTS [Tavern];
 CREATE TABLE [Tavern](
     TavernID INT IDENTITY(1, 1),
     TavernName varchar(250),
     FloorCount INT,
-    OwnerID INT,
+    UserID INT,
     LocationID INT
 );
+ALTER TABLE [Tavern]
+ADD PRIMARY KEY (TavernID);
+ALTER TABLE [Users]
+ADD PRIMARY KEY (UserID);
+ALTER TABLE [Taverns]
+ADD FOREIGN KEY (UserID) REFERENCES Users(UserID);
+ALTER TABLE [GoodsReceived]
+ADD FOREIGN KEY (TavernID) REFERENCES Tavern(TavernID);
+ALTER TABLE [Sales]
+ADD FOREIGN KEY (TavernID) REFERENCES Tavern(TavernID);
 ALTER TABLE [GoodsReceived]
 ALTER COLUMN Date date;
+ALTER TABLE [Iventory]
+ADD FOREIGN KEY (TavernID) REFERENCES Tavern(TavernID);
 ALTER TABLE [Inventory]
 ALTER COLUMN DateOfInventory date;
-DROP TABLE IF EXISTS [Supply];
-CREATE TABLE [Supply](
-	SupplyID INT IDENTITY(1, 1),
-	SupplyName varchar(250),
-	SupplyCost INT
+ALTER TABLE [Users]
+ADD FOREIGN KEY (RoleID) REFERENCES Role(RoleID);
+DROP TABLE IF EXISTS GuestStatus;
+CREATE TABLE GuestStatus(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	Status varchar(250)
 );
-DROP TABLE IF EXISTS [Role];
-CREATE TABLE [Role](
-	RoleID INT IDENTITY(1, 1),
-	RoleName varchar(250)
+DROP TABLE IF EXISTS Class;
+CREATE TABLE Class(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	ClassType varchar(250)
+);
+DROP TABLE IF EXISTS Guests;
+CREATE TABLE Guests(
+	GuestID INT PRIMARY KEY IDENTITY(1,1),
+	Names varchar(250),
+	Notes varchar(250),
+	Birthday date,
+	Cakeday date,
+	GuestStatus INT FOREIGN KEY REFERENCES GuestStatus(ID),
+);
+DROP TABLE IF EXISTS GuestClass;
+CREATE TABLE GuestClass(
+	GuestID INT FOREIGN KEY REFERENCES Guests(GuestID),
+	Class INT FOREIGN KEY REFERENCES Class(ID),
+	ClassLevel INT
 );
 INSERT INTO [GoodsReceived]
 VALUES (1,1,10,10,'2022-02-23')
@@ -127,13 +169,13 @@ VALUES ('Discounted')
 INSERT INTO [Supply]
 VALUES ('Sword',50)
 INSERT INTO [Supply]
-VALUES ('Mace',60)
+VALUES ('Bandages',3)
 INSERT INTO [Supply]
-VALUES ('Scythe',55)
+VALUES ('Hay',2)
 INSERT INTO [Supply]
-VALUES ('Booze',5)
+VALUES ('Knife',5)
 INSERT INTO [Supply]
-VALUES ('Mead',4)
+VALUES ('Map',10)
 INSERT INTO [Users]
 VALUES ('John-Jacob',1)
 INSERT INTO [Users]
@@ -154,3 +196,53 @@ INSERT INTO [Role]
 VALUES ('Secretary')
 INSERT INTO [Role]
 VALUES ('Blacksmith')
+INSERT INTO [ServiceSupplies]
+VALUES (1,1)
+INSERT INTO [ServiceSupplies]
+VALUES (2,2)
+INSERT INTO [ServiceSupplies]
+VALUES (3,4)
+INSERT INTO [ServiceSupplies]
+VALUES (4,5)
+INSERT INTO [ServiceSupplies]
+VALUES (5,3)
+INSERT INTO [GuestStatus]
+VALUES ('Healthy')
+INSERT INTO [GuestStatus]
+VALUES ('Sickly')
+INSERT INTO [GuestStatus]
+VALUES ('Injured')
+INSERT INTO [GuestStatus]
+VALUES ('Timid')
+INSERT INTO [GuestStatus]
+VALUES ('Aggressive')
+INSERT INTO [Class]
+VALUES ('Fighter')
+INSERT INTO [Class]
+VALUES ('Mage')
+INSERT INTO [Class]
+VALUES ('Rogue')
+INSERT INTO [Class]
+VALUES ('Healer')
+INSERT INTO [Class]
+VALUES ('Tank')
+INSERT INTO [Guests]
+VALUES ('Ulys-Hoakman','Very-quiet','1996-09-09','2018-04-28',4)
+INSERT INTO [Guests]
+VALUES ('Hammer-Phrok','Massive','1981-10-01','2015-02-14',5)
+INSERT INTO [Guests]
+VALUES ('Gen-Krim','Seems-trustworthy','1999-05-10','2020-07-06',1)
+INSERT INTO [Guests]
+VALUES ('Havert-Aik','Methodical','1998-09-21','2017-05-26',1)
+INSERT INTO [Guests]
+VALUES ('Owen-Wince','Missing-an-arm','1974-11-09','2009-08-07',3)
+INSERT INTO [GuestClass]
+VALUES (1,1,13)
+INSERT INTO [GuestClass]
+VALUES (1,3,8)
+INSERT INTO [GuestClass]
+VALUES (2,5,40)
+INSERT INTO [GuestClass]
+VALUES (3,4,34)
+INSERT INTO [GuestClass]
+VALUES (3,2,28)
